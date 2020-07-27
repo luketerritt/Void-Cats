e447ec47af57375355cfb1991742c04176f7dcc4
SirMaxWell <50089794+SirMaxWell@users.noreply.github.com>
2020-07-27 12:29:04 +1000

    Tab System-Currently working
    - Imported
    Tab System Scripts
    Panel Open Script
    - Changed
    Journal Prefab - added two empty objects to separate the tabs and the pages

---
+++ Void Cats/Assets/Scripts/Journal/PanelOpen.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanelOpen : MonoBehaviour
+{
+    public GameObject Panel;
+
+    public void OpenPanel()
+    {
+        if(Panel != null)
+        {
+            bool isActive = Panel.activeSelf;
+            Panel.SetActive(!isActive);
+        }
+    }
+
+     void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.Tab))
+        {
+            OpenPanel();
+        }
+    }
+
+   
+}
---
+++ Void Cats/Assets/Scripts/Journal/TabButton.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(Image))]
+public class TabButton : MonoBehaviour, IPointerEnterHandler, IPointerClickHandler, IPointerExitHandler
+{
+    public TabGroup tabGroup;
+    public Image background;
+
+    public UnityEvent onTabSelected;
+    public UnityEvent onTabDeselected;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        background = GetComponent<Image>();
+            tabGroup.Subscribe(this);
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    public void Select()
+    {
+        if(onTabSelected != null)
+        {
+            onTabSelected.Invoke();
+        }
+    }
+    public void Deselect()
+    {
+        if(onTabDeselected != null)
+        {
+            onTabDeselected.Invoke();
+        }
+    }
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        tabGroup.OnTabSelected(this);
+    }
+
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        tabGroup.OnTabEnter(this);
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        tabGroup.OnTabExit(this);
+    }
+}
