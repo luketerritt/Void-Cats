3b93f84bbc74d3ecb8070a1a0ee333720e90b057
SirMaxWell <50089794+SirMaxWell@users.noreply.github.com>
2020-07-27 15:21:00 +1000

    Player Movement and jounal in proof of concept

---
+++ Void Cats/Assets/Scripts/PlayerMovement/PlayerController3D.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController3D : MonoBehaviour
+{
+    
+    public float walkSpeed = 2; //walk speed
+    public float runSpeed = 6; // run speed
+
+    public float gravity = -12;
+    public float jumpHeight = 1;
+    [Range(0,1)]
+    public float airControlPercent;
+
+
+    public float turnSmoothTime = 0.2f; // amount of time it takes for the smoothdamp to go from current value to target
+    float turnSmoothVelocity; // is just feed to the calucations
+
+    public float speedSmoothTime = 0.1f; // same ^ 
+    float speedSmoothVelocity;
+    float currentSpeed;
+
+    float velocityY;
+
+    Animator animator;
+    Transform cameraT;
+    CharacterController controller;
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        cameraT = Camera.main.transform; // make sure what ever camera u want to use is set to main camera in tags
+        controller = GetComponent<CharacterController>();
+    }
+
+   
+    void Update()
+    {
+        // Basic Input 
+        Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        // normalise input
+        Vector2 inputDir = input.normalized;
+        //shifting Running
+        bool running = Input.GetKey(KeyCode.LeftShift);
+        Move(inputDir, running);
+        
+        if(Input.GetKeyDown (KeyCode.Space))
+        {
+            jump();
+        }
+
+        // controlling the speed percent in the animator - idle - walk - run
+        float animationSpeedPercent = ((running) ? currentSpeed / runSpeed : currentSpeed / walkSpeed * .5f);
+        // changing the float in the animator
+        animator.SetFloat("SpeedPercent", animationSpeedPercent, speedSmoothTime, Time.deltaTime);
+
+    }
+     void Move(Vector2 inputDir, bool running)
+    {
+        if (inputDir != Vector2.zero) // avoids character snapping back to 0 degrees
+        {
+            // Player Rotation 
+            float targetRotation = Mathf.Atan2(inputDir.x, inputDir.y) * Mathf.Rad2Deg + cameraT.eulerAngles.y;
+            // smooths the turning/rotation of the character 
+            transform.eulerAngles = Vector3.up * Mathf.SmoothDampAngle(transform.eulerAngles.y, targetRotation,
+                ref turnSmoothVelocity, GetModifiedSmoothTime(turnSmoothTime));
+
+        }
+
+       
+        float targetspeed = ((running) ? runSpeed : walkSpeed) * inputDir.magnitude;
+        // smoothing speed control
+        currentSpeed = Mathf.SmoothDamp(currentSpeed, targetspeed, ref speedSmoothVelocity, GetModifiedSmoothTime(speedSmoothTime));
+
+        velocityY += Time.deltaTime * gravity;
+
+        // Moves the character to face the right direction
+        Vector3 velocity = transform.forward * currentSpeed + Vector3.up * velocityY;
+
+        controller.Move(velocity * Time.deltaTime);
+        currentSpeed = new Vector2(controller.velocity.x, controller.velocity.z).magnitude;
+
+        if (controller.isGrounded)
+        {
+            velocityY = 0;
+        }
+
+       
+    }
+    void jump()
+    {
+        if(controller.isGrounded)
+        {
+            float jumpVelocity = Mathf.Sqrt(-2 * gravity * jumpHeight);
+            velocityY = jumpVelocity;
+        }
+    }
+    float GetModifiedSmoothTime(float smoothTime)
+    {
+        if(controller.isGrounded)
+        {
+            return smoothTime;
+        }
+        if(airControlPercent == 0)
+        {
+            return float.MaxValue;
+        }
+        return smoothTime / airControlPercent;
+    }
+}
---
+++ Void Cats/Assets/Scripts/PlayerMovement/ThirdPersonCamera.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThirdPersonCamera : MonoBehaviour
+{
+    public bool lockCursor;
+    public float mouseSensitivity = 10;
+    public Transform target; // player
+    public float distancefromTarget = 2; // distance from target
+    float yaw; // Y axis
+    float pitch; // X axis
+
+    public Vector2 pitchMinMax = new Vector2(-40, 85);
+    public float rotationSmoothTime = 0.12f;
+    Vector3 rotationSmmothVelocity;
+    Vector3 currentRotation;
+    void Start()
+    {
+        //locks cursor...(duh)
+        if (lockCursor)
+        {
+            Cursor.lockState = CursorLockMode.Locked;
+            Cursor.visible = false;
+        }
+    }
+
+    void LateUpdate()
+    {
+        // Basic Input
+        yaw += Input.GetAxis("Mouse X") * mouseSensitivity;
+        pitch -= Input.GetAxis("Mouse Y") * mouseSensitivity;
+        // clamping the camera on the x axis
+        pitch = Mathf.Clamp(pitch, pitchMinMax.x, pitchMinMax.y);
+        //Rotation(smooths the rotation aswell)
+        currentRotation = Vector3.SmoothDamp(currentRotation, new Vector3(pitch, yaw), ref rotationSmmothVelocity, rotationSmoothTime);
+        transform.eulerAngles = currentRotation;
+        //
+        transform.position = target.position - transform.forward * distancefromTarget;
+    }
+
+    
+}
