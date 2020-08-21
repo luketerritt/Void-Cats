00f00e435420f5e5ef2f639bf8158cda4734fe7e
Merve <mervenur.soylu@gmail.com>
2020-08-21 16:32:23 +1000

    Added Camera Flash Animation with basic code

---
+++ Void Cats/Assets/Scripts/CameraFlash.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFlash : MonoBehaviour
+{
+    public Animator AnimPanel;
+    private bool canAnimate = false;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            canAnimate = true;
+        }
+
+        if(canAnimate == true)
+        {
+            AnimPanel.SetBool("CameraFlash", true);
+        }
+    }
+
+    public void StopFlashAnim()
+    {
+        AnimPanel.SetBool("CameraFlash", false);
+        canAnimate = false;
+    }
+}
