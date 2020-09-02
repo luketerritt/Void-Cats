4d7edf81819e76724e93a71616dc5eab77331a59
Matthew <lynbrookmatt@outlook.com>
2020-09-02 12:54:11 +1000

    Updated Tide, removed old Matthew Prototype

--- Void Cats/Assets/Scripts/TideMovement.cs
+++ Void Cats/Assets/Scripts/TideMovement.cs
@@ -18,9 +18,12 @@ public class TideMovement : MonoBehaviour
     //private float downIteratorFrozen = 0;
 
     //when should the tide be up, false for day, true for night
-    private bool nightUp = false;
+    //private bool nightUp = false;
 
-    private bool freezeTide = false;
+    //have this on to have original tide with no smoothing
+    public bool stopAtPeak = false;
+
+    public float drownDetectionOffSet = 1;
 
     // Start is called before the first frame update
     void Start()
@@ -39,11 +42,11 @@ public class TideMovement : MonoBehaviour
         //numbers to try to help understand logic of going down - thinking via typing...
         //13,14,15,16,17,18,19,20,21,22,23,24 - time
         //2,  4, 6, 8,10,12,14,16,18,20,22,24 - number being fed in?
-        //11,10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 - wanted output
+        //11,10, 9, 8, 7, 6, 5, 4, 3, 2, 1, 0 - wanted output?
 
         //if the tide should be up during the day
-        if(!nightUp)
-        {
+        //if(!nightUp)
+        //{
             //if it is in the afternoon, reverse a temp time, so the tide can go out
             if (time >= 12)
             {
@@ -54,8 +57,8 @@ public class TideMovement : MonoBehaviour
             {
                 downIterator = 0;
             }
-        }
-        else
+        //}
+        /*else
         {
             //if it is in the morning, reverse a temp time, so the tide can go out
             if (time <= 12)
@@ -67,10 +70,10 @@ public class TideMovement : MonoBehaviour
             {
                 downIterator = 0;
             }
-        }
+        }*/
 
-        //if the tide is not frozen, do math calculations -- ONLY SAFE TO FREEZE IN first half of day
-        if (!freezeTide)
+        //if this is the original version that linearly moves
+        if(stopAtPeak)
         {
             //math to change the scale of the range and update what the currentheight should be
             float oldRange = 24;
@@ -82,8 +85,71 @@ public class TideMovement : MonoBehaviour
                 new Vector3(this.gameObject.transform.position.x, (currentHeight * 2), this.gameObject.transform.position.z);
 
         }
+        else //if this is the experimental version that "smoothly" stops
+        {
+            //if the tide is greater than 1 and less than 11, 
+            if (time > 2 && time < 10)
+            {
+
+                //math to change the scale of the range and update what the currentheight should be
+                float oldRange = 24;
+                float NewRange = maxHeight - minHeight;
+                currentHeight = (((time) * NewRange) / oldRange) + minHeight;
+
+                //update position of the tide
+                this.gameObject.transform.position =
+                    new Vector3(this.gameObject.transform.position.x, (currentHeight * 2), this.gameObject.transform.position.z);
 
 
+                //Debug.Log("Regular tide time = " + time);
+            }
+            else
+            {
+                //if the time is less than 1 or bigger than 11 
+                if (time < 0.5f || time > 11.5f)
+                {
+                    //do nothing and be still
+                    //Debug.Log("Tide should be frozen, time = " + time);
+                }
+                else //attempt to be smoother
+                {
+                    time -= 0.9f * Time.deltaTime;
+                    //math to change the scale of the range and update what the currentheight should be
+                    float oldRange = 24;
+                    float NewRange = maxHeight - minHeight;
+                    currentHeight = (((time) * NewRange) / oldRange) + minHeight;
 
+                    //update position of the tide
+                    this.gameObject.transform.position =
+                        new Vector3(this.gameObject.transform.position.x, (currentHeight * 2), this.gameObject.transform.position.z);
+
+                    //Debug.Log("Tide smoothing attempts, time = " + time);
+                }
+
+
+            }
+        }      
+
+    }
+
+    //if something with a rigidbody enters the water (character controller counts)
+    private void OnTriggerStay(Collider other)
+    {
+        //if the player enters the water
+        if(other.CompareTag("Player"))
+        {
+            //get the players first person camera
+            var temp = other.gameObject.GetComponent<PlayableCamera>().firstPersonCamera;
+           
+            //if the camera's y is less than or equal to the tides y
+            if((temp.transform.position.y - drownDetectionOffSet) <= this.transform.position.y)
+            {
+                Debug.Log("Player should be teleported away");
+            }
+
+            
+        }
+
+        
     }
 }
