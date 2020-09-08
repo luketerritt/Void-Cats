d9d6b686895bd3dc1d162b7ad0b4b318b523151a
Matthew <lynbrookmatt@outlook.com>
2020-09-08 11:51:14 +1000

    Updated BushInteraction, added early main menu code
    smooth transition for entering and leaving the bush

--- Void Cats/Assets/Scripts/Interaction System/BushInteraction.cs
+++ Void Cats/Assets/Scripts/Interaction System/BushInteraction.cs
@@ -21,8 +21,16 @@ public class BushInteraction : MonoBehaviour, IInteractable
 
     public GameObject LeavesUI;
 
+    //gets set to true during the time the player enters or leaves the bush
+    private bool bushTransition = false;
+
     //offset used when teleporting the player into the bush so their head sticks out the top
     public float heightOffset = 2;
+
+    public float transitionTime = 2;
+    private float t = 0;
+    private float overallDistance = 0;
+    
 
     void Start()
     {
@@ -45,18 +53,90 @@ public class BushInteraction : MonoBehaviour, IInteractable
             if(Input.GetKeyDown(KeyCode.Escape))
             {
                 //relocate the player back to where they were before they went into the bush
-                RelocateToBush(player.GetComponent<PlayerController3D>().controller,
-                    player.gameObject.GetComponent<PlayerController3D>().trackedPosition);
+                //RelocateToBush(player.GetComponent<PlayerController3D>().controller,
+                //player.gameObject.GetComponent<PlayerController3D>().trackedPosition);
 
                 //turn back on the player controller
-                player.GetComponent<PlayerController3D>().controller.enabled = true;
+                //player.GetComponent<PlayerController3D>().controller.enabled = true;
 
-                player.gameObject.GetComponent<PlayerController3D>().inBush = false;
+                //player.gameObject.GetComponent<PlayerController3D>().inBush = false;
+                Vector3 temp = player.GetComponent<PlayerController3D>().trackedPosition;
+                overallDistance = Vector3.Distance(player.GetComponent<PlayerController3D>().controller.transform.position, temp);
+                bushTransition = true;
                 playerInThisBush = false;
-                LeavesUI.SetActive(false);
-                thisCollider.enabled = true;
+                //LeavesUI.SetActive(false);
+                //thisCollider.enabled = true;
             }
         }
+
+        //if we are transitioning into the bush
+        if(bushTransition)
+        {
+            //if we are going in
+            if(playerInThisBush)
+            {
+                t += Time.deltaTime / transitionTime;
+
+                Vector3 temp = new Vector3(this.gameObject.transform.position.x,
+                    this.gameObject.transform.position.y + heightOffset,
+                    this.gameObject.transform.position.z);
+
+                player.GetComponent<PlayerController3D>().controller.enabled = false;
+                player.GetComponent<PlayerController3D>().controller.transform.position
+                    = Vector3.Lerp(player.GetComponent<PlayerController3D>().controller.transform.position
+                    , temp, t);
+
+                float distance = Vector3.Distance(player.GetComponent<PlayerController3D>().controller.transform.position, temp);
+
+                //Debug.Log("current distance = " + distance + " , overall distance = " + overallDistance);
+
+                //if we are halfway
+                if ((overallDistance * 0.5f) > distance)
+                {
+                    LeavesUI.SetActive(true);
+                }
+
+                //we are in the bush!
+                if (distance < 0.1f)
+                {
+                    bushTransition = false;
+                    t = 0;
+                }
+            }
+            else
+            {
+                t += Time.deltaTime / transitionTime;
+
+                Vector3 temp = player.GetComponent<PlayerController3D>().trackedPosition;
+
+                player.GetComponent<PlayerController3D>().controller.enabled = false;
+                player.GetComponent<PlayerController3D>().controller.transform.position
+                    = Vector3.Lerp(player.GetComponent<PlayerController3D>().controller.transform.position
+                    , temp, t);
+
+                float distance = Vector3.Distance(player.GetComponent<PlayerController3D>().controller.transform.position, temp);
+
+                //Debug.Log("current distance = " + distance + " , overall distance = " + overallDistance);
+
+                //if we are halfway
+                if((overallDistance * 0.5f) > (distance))
+                {
+                    LeavesUI.SetActive(false);
+                }
+
+                //we are no longer in the bush
+                if(distance < 0.1f)
+                {
+                    bushTransition = false;
+                    t = 0;
+                    player.gameObject.GetComponent<PlayerController3D>().inBush = false;
+                    player.GetComponent<PlayerController3D>().controller.enabled = true;
+                    thisCollider.enabled = true;
+                    //LeavesUI.SetActive(false);
+                }
+            }
+        }
+
     }
 
 
@@ -88,15 +168,15 @@ public class BushInteraction : MonoBehaviour, IInteractable
             if(playerInThisBush)
             {
                 //relocate the player back to where they were before they went into the bush
-                RelocateToBush(player.GetComponent<PlayerController3D>().controller,
-                    player.gameObject.GetComponent<PlayerController3D>().trackedPosition);
-
+                //RelocateToBush(player.GetComponent<PlayerController3D>().controller,
+                //player.gameObject.GetComponent<PlayerController3D>().trackedPosition);
+                bushTransition = true;
                 //turn back on the player controller
-                player.GetComponent<PlayerController3D>().controller.enabled = true;
+                //player.GetComponent<PlayerController3D>().controller.enabled = true;
 
                 player.gameObject.GetComponent<PlayerController3D>().inBush = false;
                 playerInThisBush = false;
-                LeavesUI.SetActive(false);
+                //LeavesUI.SetActive(false);
                 thisCollider.enabled = true;
             }
             else //enter the bush
@@ -106,18 +186,26 @@ public class BushInteraction : MonoBehaviour, IInteractable
                 currentAmount = 0.0f; // resets the circle to 0
                 isProgressImageOn = false;
                 interactTextUi.SetActive(false); // turn off text
-                                                 //playableCameraScript.isCursorLocked = false; // unlocks the cursor for the player to select the buttons 
-                Debug.Log("player should enter bush");
+                //playableCameraScript.isCursorLocked = false; // unlocks the cursor for the player to select the buttons 
+
+                //Debug.Log("player should enter bush");
                 player.gameObject.GetComponent<PlayerController3D>().inBush = true;
+
                 playerInThisBush = true;
+
                 //also force a tracker backup for the player!
                 player.gameObject.GetComponent<PlayerController3D>().trackedPosition = player.gameObject.transform.position;
 
-                Vector3 temp = new Vector3(this.gameObject.transform.position.x, this.gameObject.transform.position.y + heightOffset, this.gameObject.transform.position.z);
+                Vector3 temp = new Vector3(this.gameObject.transform.position.x,
+                    this.gameObject.transform.position.y + heightOffset,
+                    this.gameObject.transform.position.z);
 
-                RelocateToBush(player.GetComponent<PlayerController3D>().controller, temp);
+                overallDistance = Vector3.Distance(player.GetComponent<PlayerController3D>().controller.transform.position, temp);
+                //RelocateToBush(player.GetComponent<PlayerController3D>().controller, temp);
 
-                LeavesUI.SetActive(true);
+                bushTransition = true;
+
+                //LeavesUI.SetActive(true);
                 thisCollider.enabled = false;
             }
             
---
+++ Void Cats/Assets/Scripts/MainMenuButtons.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+using UnityEngine;
+
+public class MainMenuButtons : MonoBehaviour, IPointerClickHandler
+{
+
+
+    //int determines button type
+    public int buttonType = 0;
+    //instance of button refers to itself
+
+    //when a button is clicked
+    public void OnPointerClick(PointerEventData pointerEventData)
+    {
+
+        switch(buttonType)
+        {
+            case 0: //exit application
+                {
+                    if (pointerEventData.button == PointerEventData.InputButton.Left)
+                    {
+                        ExitApplication();
+                    }
+                    break;
+                }
+            case 1: //return to main menu
+                {
+                    if (pointerEventData.button == PointerEventData.InputButton.Left)
+                    {
+                        GoToMainMenu();
+                    }
+                    break;
+                }
+            case 2: //go to game scene -- will be revamped later!
+                {
+                    if (pointerEventData.button == PointerEventData.InputButton.Left)
+                    {
+                        GoToGameScene();
+                    }
+                    break;
+                }
+        }
+        //if the button is of type 0, this is the Exit application Button
+        //if (buttonType == 0)
+        //{
+        //    //if you have been left clicked
+        //    if (pointerEventData.button == PointerEventData.InputButton.Left)
+        //    {
+        //        ExitApplication();
+        //    }
+        //}
+
+        //if the button is of type 1, this is quit to menu button (from ingame to main menu
+        //if (buttonType == 1)
+        //{
+        //    //if you have been left clicked
+        //    if (pointerEventData.button == PointerEventData.InputButton.Left)
+        //    {                
+        //        GoToMainMenu();
+        //    }
+        //}
+
+    }
+
+    void ExitApplication()
+    {
+        //exit application
+        Application.Quit();
+        Debug.Log("test message");
+    }
+
+    void GoToMainMenu()
+    {
+        //go to the main menu (has to be added in the build settings)
+        SceneManager.LoadScene("Menu");
+        Debug.Log("going to main menu");
+    }
+
+    void GoToGameScene()
+    {
+        //go to the main scene
+        SceneManager.LoadScene("MainScene");
+        Debug.Log("going to game scene");
+    }
+
+}
